f43c2398e25c37c8a00563cc49c749711cc03061
HeshamMoawad <heshammoawad120120@yahoo.com>
2023-02-28 14:55:55 +0200

    restyle app

--- styles.py
+++ styles.py
@@ -44,12 +44,13 @@ class Styles():
     class Backgrounds():
         Transparent = f"background-color:transparent;"
         Orange = "background-color:rgb(255, 112, 16);color:black;"
-        DarkOrange = """background-color: black;color:white;"""
+        DarkOrange = """background-color:black;color:white;"""
         LineEdit = """"background-color:gray;color:black;"""
+        Blue = "background-color:blue;color:white;"
+        gradiant = """background-color:qlineargradient(spread:pad, x1:0.943, y1:0.733591, x2:0.131, y2:0.500227, stop:0 rgba(0, 0, 0, 210), stop:1 rgba(0, 68, 150, 255));"""
+        Black = """background-color:black;color:white;"""
+        White = """background-color:lightgray;color:black;"""
 
-    class AnimationToggle():
-        Orange = ''
-    
     class LineEdit():
         Normal = """
         QLineEdit{
@@ -69,9 +70,9 @@ class Styles():
     class Widget():
         Normal = """
         QWidget{
-            font:14px;
+            font:16px;
             color:white;
-            background-color:  qlineargradient(spread:pad, x1:0.943, y1:0.733591, x2:0.131, y2:0.500227, stop:0 rgba(0, 0, 0, 210), stop:1 rgba(7, 107, 230, 217));
+            background-color:qlineargradient(spread:pad, x1:0.943, y1:0.733591, x2:0.131, y2:0.500227, stop:0 rgba(0, 0, 0, 210), stop:1 rgba(0, 68, 150, 255));
         }
         """
     class ComboBox():
@@ -134,6 +135,14 @@ class Styles():
         }
         QFrame{
             background-color:transparent;
+        }
+        """
+        custom = """
+        MyQFrame{
+            background-color:blue;
+        }
+        QFrame{
+            background-color:blue;
         }
         """
     class TreeWidget():
